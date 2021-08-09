49d8fd8da3a1229dfe26f5fff88fe690fab7f555
Andreas Kloeckner <inform@tiker.net>
2021-08-08 19:11:39 -0500

    Update cropped images in make.sh

--- notes/make.sh
+++ notes/make.sh
@@ -2,10 +2,27 @@
 
 set -e
 
+update_cropped_images()
+{
+    for i in *.pdf; do
+        if [[ $i == *"-crop.pdf" ]]; then
+            continue
+        fi
+        outf="${i%.pdf}-crop.pdf"
+        if test "$i" -nt "$outf"; then
+            pdfcrop "$i" "$outf"
+        fi
+    done
+}
+
+(cd images; update_cropped_images)
+
 if [[ "$1" = "watch" ]]; then
-  git ls-files | entr ./run-org-conversion.sh
+    git ls-files | entr ./run-org-conversion.sh
 else
-  ./run-org-conversion.sh
+    ./run-org-conversion.sh
 fi
 
 echo "NOTES BUILD SCCESSFULLY COMPLETED"
+
+# vim: sw=4
